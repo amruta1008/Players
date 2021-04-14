043e42bfe91440d70e034698e1e930d8683a84eb
amruta100 <bramruta9@gmail.com>
2021-04-14 16:01:36 +0530

    first - commit

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -7,50 +7,44 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Map<String,String[]> map = new HashMap<String,String[]>();
+		Map<String, String[]> map = new HashMap<String, String[]>();
 		Scanner sc = new Scanner(System.in);
-		gh: while(true) {
-		System.out.println("enter the player name");
-		String Pname = sc.next();
-		System.out.println("enter the wickets seperated by |");
-		String str1 = sc.next();
-		String[] strar = str1.split("\\|");
-		map.put(Pname,strar);
-		System.out.println("Do you want to add another player(yes/no)");
-		String anPlayer = sc.next();
-		if(anPlayer.equals("yes")) 
-			continue;
-		else if(anPlayer.equals("no"))
-			
-			break gh;
+		gh: while (true) {
+			System.out.println("enter the player name");
+			String Pname = sc.nextLine();
+			System.out.println("enter the wickets seperated by |");
+			String str1 = sc.nextLine();
+			String[] strar = str1.split("\\|");
+			map.put(Pname, strar);
+			System.out.println("Do you want to add another player(yes/no)");
+			String anPlayer = sc.nextLine();
+			if (anPlayer.equals("yes"))
+				continue;
+			else if (anPlayer.equals("no"))
+
+				break gh;
 		}
-		
-		fh: while(true)
-		{
+
+		fh: while (true) {
 			System.out.println("enter the players to search");
-			String str =  sc.next();
-			if(map.containsKey(str))
-			{
-				System.out.println("Player name:"+str);
+			String str = sc.nextLine();
+			if (map.containsKey(str)) {
+				System.out.println("Player name:" + str);
 				String[] strarr = map.get(str);
 				System.out.println("Wickets:");
-				for(String str1:strarr)
+				for (String str1 : strarr)
 					System.out.println(str1);
-			}
-			else
-			{
+			} else {
 				System.out.println("NO players found");
 			}
 			System.out.println("Do You want to search for annother player(Yes/No)");
-			String anPlayer = sc.next();
-			if(anPlayer.equals("yes")) 
+			String anPlayer = sc.nextLine();
+			if (anPlayer.equals("yes"))
 				continue;
-			else if(anPlayer.equals("no"))
-				
+			else if (anPlayer.equals("no"))
+
 				break fh;
 		}
-		
-		}
+
 	}
-
-
+}
